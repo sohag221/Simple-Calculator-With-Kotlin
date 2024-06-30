cae7151c6eebb79d122b9f154caba155008a1fb7
sohag221 <mdratulsohag1234@gmail.com>
2024-06-30 18:39:31 +0600

    UI design done

---
+++ src/main/java/com/example/simplecalculatorwithkotlin/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.simplecalculatorwithkotlin
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onClearClick(view: View) {}
+    fun onBackClick(view: View) {}
+    fun onOperatorClick(view: View) {}
+    fun onDigitclick(view: View) {}
+    fun onAllClearClick(view: View) {}
+    fun onEqualClick(view: View) {}
+}
